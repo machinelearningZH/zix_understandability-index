472f78f899c540a3a2e5eb0a70efa33a27623347
Juri Opitz <59831412+flipz357@users.noreply.github.com>
2026-09-07 09:57:33 +0200

    Specify RIX implementation with an additional note
    Added a note about the RIX readability index calculation method.

--- zix/understandability.py
+++ zix/understandability.py
@@ -107,6 +107,7 @@ def _additional_metrics(doc):
 
     # Calculate RIX readability index.
     # Reference: https://github.com/HLasse/TextDescriptives/blob/main/src/textdescriptives/components/readability.py#L146
+    # Note: We use a slightly simplified version of RIX, without additional filtering of long_words
     long_words = len([token for token in doc if len(token) > 6])
     n_sentences = len(list(doc.sents))
     rix = long_words / n_sentences
